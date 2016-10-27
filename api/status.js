afdd547317c6d705f0a475f41050d6d28c4a77a9
Dong Qihong <dongqihong@vip.qq.com>
2016-10-27 20:53:15 +0800

    status api，更新内容

--- api/status.js
+++ api/status.js
@@ -2,23 +2,21 @@
 
 module.exports = (app, options) => {
   app.get('/status', (req, res, next) => {
-    var pinsCount = 0;
     options.repository.getPins().then((pins) => {
-      pinsCount = pins.length;
+      var pinsCount = pins.length;
+      options.repository.getStatus().then((status) => {
+        if (req.query.callback) {
+          res.jsonp({
+            status: status,
+            pinsCount: pinsCount
+          });
+        } else {
+          res.json({
+            status: status,
+            pinsCount: pinsCount
+          });
+        }
+      }).catch(next);
     });
-
-    options.repository.getStatus().then((status) => {
-      if (req.query.callback) {
-        res.jsonp({
-          status: status,
-          pinsCount: pinsCount
-        });
-      } else {
-        res.json({
-          status: status,
-          pinsCount: pinsCount
-        });
-      }
-    }).catch(next);
   });
 };
